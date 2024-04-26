213273b145d09e5ac3e4eb819ec7208ea0c44a69
ji <tlap@naver.com>
2024-04-26 16:21:18 +0900

    대댓글

--- DML/7차.sql
+++ DML/7차.sql
@@ -1,3 +1,17 @@
 --대댓글 tblReplyComments
+insert into tblReplyComments (seq, seqComments, seqUser, content, writeDate, likeCount, dislikeCount, reportCount) 
+values (1, 1, 1, '감사합니다.', '2024-04-28', 0, 0, 0);
+
+insert into tblReplyComments (seq, seqComments, seqUser, content, writeDate, likeCount, dislikeCount, reportCount) 
+values (2, 2, 2, '감사합니다.', '2024-04-28', 0, 0, 0);
+
+insert into tblReplyComments (seq, seqComments, seqUser, content, writeDate, likeCount, dislikeCount, reportCount) 
+values (3, 3, 6, '화이팅!', '2024-04-28', 0, 0, 0);
+
+insert into tblReplyComments (seq, seqComments, seqUser, content, writeDate, likeCount, dislikeCount, reportCount) 
+values (4, 4, 7, '문문문어어어', '2024-04-28', 0, 0, 0);
+
+insert into tblReplyComments (seq, seqComments, seqUser, content, writeDate, likeCount, dislikeCount, reportCount) 
+values (5, 6, 7, '운영자님 ㄳ', '2024-04-28', 0, 0, 0);
 
 --가계부 카테고리 목록 tblAccCategoryList
