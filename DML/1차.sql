44c449b7ab652a9102d16bb6b6e46952ee9cb856
zzzzzzin <jyj7314@naver.com>
2024-04-26 09:14:09 +0900

    DML-insert

--- DML/1차.sql
+++ DML/1차.sql
@@ -1,37 +1,45 @@
---프로필 이미지 tblProfileimg
+INSERT INTO employees (employee_id, first_name, last_name, email, hire_date)
+VALUES (101, 'John', 'Doe', 'john.doe@example.com', '2024-04-30');
 
---게시판 카테고리 tblCategory
+-- 저축 기간 tblSavingsPeriod
+insert into tblSavingsPeriod (seq, period) values (1, 1);
+insert into tblSavingsPeriod (seq, period) values (2, 2);
+insert into tblSavingsPeriod (seq, period) values (3, 3);
+insert into tblSavingsPeriod (seq, period) values (4, 4);
+insert into tblSavingsPeriod (seq, period) values (5, 5);
+insert into tblSavingsPeriod (seq, period) values (6, 6);
+insert into tblSavingsPeriod (seq, period) values (7, 7);
+insert into tblSavingsPeriod (seq, period) values (8, 8);
+insert into tblSavingsPeriod (seq, period) values (9, 9);
+insert into tblSavingsPeriod (seq, period) values (10, 10);
+insert into tblSavingsPeriod (seq, period) values (11, 11);
+insert into tblSavingsPeriod (seq, period) values (12, 12);
 
---금지어 tblBanWord
+-- 부채 tblDebt
+insert into tblDebt (seq, cash) values (1, 100000);
+insert into tblDebt (seq, cash) values (2, 200000);
+insert into tblDebt (seq, cash) values (3, 300000);
+insert into tblDebt (seq, cash) values (4, 400000);
+insert into tblDebt (seq, cash) values (5, 500000);
+insert into tblDebt (seq, cash) values (6, 600000);
+insert into tblDebt (seq, cash) values (7, 700000);
+insert into tblDebt (seq, cash) values (8, 800000);
+insert into tblDebt (seq, cash) values (9, 900000);
+insert into tblDebt (seq, cash) values (10, 1000000);
 
---배너 이미지 tblBannerImage
+-- 자산 tblProperty
+insert into tblProperty (seq, cash) values (1, 100000);
+insert into tblProperty (seq, cash) values (2, 200000);
+insert into tblProperty (seq, cash) values (3, 300000);
+insert into tblProperty (seq, cash) values (4, 400000);
+insert into tblProperty (seq, cash) values (5, 500000);
+insert into tblProperty (seq, cash) values (6, 600000);
+insert into tblProperty (seq, cash) values (7, 700000);
+insert into tblProperty (seq, cash) values (8, 800000);
+insert into tblProperty (seq, cash) values (9, 900000);
+insert into tblProperty (seq, cash) values (10, 1000000);
 
---로그 tblLog
+-- 입출금 상태 tblDepositWithdrawalStatus
+insert into tblDepositWithdrawalStatus (seq, status) values (1, '입금');
+insert into tblDepositWithdrawalStatus (seq, status) values (2, '출금');
 
---압박 강도 tblCompressionIntensity
-
---저축 기간 tblSavingsPeriod
-
---부채 tblDebt
-
---자산 tblProperty
-
---API tblAPI
-
---입출금 상태 tblDepositWithdrawalStatus
-
---카드 카테고리 tblCardCategory
-
---가계부 카테고리  tblAccCategory
-
---뉴스 tblNews
-
---고정 변동 기간 tblFixedFluctuationPeriod
-
---변동 사유 목록  tblReasonsChangeList
-
---권한 tblPriv
-
---관리자 tblAdmin
-
---카드 타입 tblCardType
