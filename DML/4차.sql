81134f29163ae3a913655859a5b7a35460b63e42
VVJD <hj828@kakao.com>
2024-04-26 14:23:00 +0900

    DML 수정

--- DML/4차.sql
+++ DML/4차.sql
@@ -3,7 +3,38 @@
 --회원 금융 정보 tblMemberFinance
 
 --나의 카드 tblMyCard
+select * from tblMember;
+select * from tblCardInformation;
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (1, '4005550000000001', '토뱅', '2026-05-03', 'abc001@naver.com', 1);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (2, '4012888888881881', '신한 드림', '2030-03-20', 'abc001@naver.com', 9);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (3, '4111111111111111', '국민 알뜰', '2028-05-26', 'abc002@naver.com', 5);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (4, '4444333322221111', '노리 체크', '2032-08-20', 'abc002@naver.com', 3);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (5, '4444333322220000', '노리 체크', '2032-08-20', 'abc003@naver.com', 3);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (6, '4555400000555123', '오하쳌', '2026-10-10', 'abc002@naver.com', 2);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (7, '4564456445644564', '오하쳌', '2030-05-12', 'abc004@naver.com', 2);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (8, '4716914706534228', '국민 토심이', '2030-05-12', 'abc004@naver.com', 7);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (9, '4916541713757159', '신한 라이프', '2027-01-12', 'abc001@naver.com', 6);
+insert into tblMyCard (seq, cardNumber, alias, validity, idMember, seqCardInformation) 
+    values (10, '4916615639346972', '농협 flex', '2034-01-10', 'abc004@naver.com', 8);
 
 --가계부 tblAcc
 
 --회원 권한 목록 tblMemberPriv
+--권한(1: 비회원, 2:회원, 3:관리자, 4:탈퇴)
+insert into tblMemberPriv (seq, seqPriv, idMember) 
+    values (1, 1, 'abc001@naver.com');
+insert into tblMemberPriv (seq, seqPriv, idMember) 
+    values (2, 2, 'abc002@naver.com');
+insert into tblMemberPriv (seq, seqPriv, idMember) 
+    values (3, 2, 'abc003@naver.com');
+insert into tblMemberPriv (seq, seqPriv, idMember) 
+    values (4, 4,'abc004@naver.com');
---
+++ DML/6차.sql
@@ -0,0 +1,5 @@
+--첨부 파일 tblAttachedFile
+
+--댓글 tblComments
+
+--가계부 카테고리 목록 tblAccCategoryList
