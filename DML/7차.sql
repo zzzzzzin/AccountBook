72a49a01319a2cce6aa5157b7706a9cd0b5f3253
SXNGJUNHX <untitlednamenull@naver.com>
2024-04-26 16:02:15 +0900

    tblAccCategoryList update
    tblAccCategoryList update

--- DML/7차.sql
+++ DML/7차.sql
@@ -1,3 +1,14 @@
 --대댓글 tblReplyComments
 
 --가계부 카테고리 목록 tblAccCategoryList
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (1,33,1);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (2,35,2);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (3,8,3);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (4,39,4);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (5,2,5);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (6,38,6);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (7,5,7);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (8,35,8);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (9,33,9);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (10,35,10);
+INSERT INTO tblAccCategoryList(seq,seqAccCategory,seqAccInfo) VALUES (11,5,11);
