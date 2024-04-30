1b2157ca23d9f9c952610b517f2a9370fe6e732b
SXNGJUNHX <untitlednamenull@naver.com>
2024-04-30 10:32:44 +0900

    board update
    board update

---
+++ code/src/main/java/com/project/accountbook/board/freeBoard.java
@@ -0,0 +1,24 @@
+package com.project.accountbook.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/freeBoard.do")
+public class freeBoard extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/freeBoard.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+}
