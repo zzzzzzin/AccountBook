82de13fedb6cbed6b4cf35a1c774060d1f7c3b74
juniel1299 <youngs5440@gmail.com>
2024-05-02 16:39:48 +0900

    관리자 회원 로그인

--- code/src/main/java/com/project/accountbook/user/Login.java
+++ code/src/main/java/com/project/accountbook/user/Login.java
@@ -23,27 +23,37 @@ public class Login extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//LoginOk.java 역할
-//1. 데이터 가져오기(id, pw)
-//2. DB 작업 > select
-//3. 결과 > 인증 티켓
-		String id = req.getParameter("id");
-		String pw = req.getParameter("pw");
-		UserDAO dao = new UserDAO();
-		UserDTO dto = new UserDTO();
-		dto.setId(id);
-		dto.setPw(pw);
-//int result = dao.login(dto); //1, 0
-		UserDTO result = dao.login(dto);// dto, null
-		if (result != null && result.getId() != null) {
-			HttpSession session = req.getSession();
-			session.setAttribute("id", result.getId());
-			resp.sendRedirect("/account/index.do"); // 로그인 성공 후 리다이렉트할 페이지 경로
-		} else {
-			resp.setCharacterEncoding("UTF-8");
-			PrintWriter writer = resp.getWriter();
-			writer.print(OutputUtil.redirect("로그인에 실패했습니다."));
-			writer.close();
-		}
+	    String id = req.getParameter("id");
+	    String pw = req.getParameter("pw");
+
+	    UserDAO dao = new UserDAO();
+	    UserDTO dto = new UserDTO();
+	    dto.setId(id);
+	    dto.setPw(pw);
+
+	    UserDTO result = dao.login(dto);
+
+	    if (result != null && result.getId() != null) {
+	        HttpSession session = req.getSession();
+	        session.setAttribute("id", result.getId());
+	        session.setAttribute("seqPriv", result.getSeqPriv());
+
+	        resp.sendRedirect("/account/index.do");
+	    } else {
+	        result = dao.loginAdmin(dto);
+
+	        if (result != null && result.getId() != null) {
+	            HttpSession session = req.getSession();
+	            session.setAttribute("id", result.getId());
+	            session.setAttribute("seqPriv", result.getSeqPriv());
+
+	            resp.sendRedirect("/account/index.do");
+	        } else {
+	            resp.setCharacterEncoding("UTF-8");
+	            PrintWriter writer = resp.getWriter();
+	            writer.print(OutputUtil.redirect("로그인에 실패했습니다."));
+	            writer.close();
+	        }
+	    }
 	}
 }
